b30e216970947f0c0b29e84155f500f5dd95407e
Phan Sĩ Thành <thanhphanscpy2005@gmail.com>
2026-05-31 09:58:25 +0700

    init project

---
+++ src/main/java/clc65/ithanhphan/cuoiki/CuoikiApplication.java
@@ -0,0 +1,13 @@
+package clc65.ithanhphan.cuoiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuoikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuoikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clc65/ithanhphan/cuoiki/CuoikiApplicationTests.java
@@ -0,0 +1,13 @@
+package clc65.ithanhphan.cuoiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuoikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
